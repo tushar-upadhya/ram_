e8d3b505e913ff12b4d4d90e7b34d5ab2e2848d2
tushar upadhyay <tusharupadhyay690@gmail.com>
2025-02-27 14:27:36 +0530

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Faq from "@/components/faq/Faq";
+import Hero from "@/components/hero/Hero";
 import React from "react";
 
 const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto">
+      <Hero />
       <Faq />
     </div>
   );
---
+++ components/hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero: React.FC = () => {
+  return (
+    <div className="">
+      <img
+        src="https://cdn.builder.io/api/v1/image/assets/bc5e7ff961d64170a9f81ec1a5481f4b/4358dcf887d021b68417d72f9fe6ae4981575de7d828a326c8bc98d53cd2ce6e?placeholderIfAbsent=true"
+        alt="Content image"
+        className="aspect-[2.35] w-full object-contain"
+      />
+    </div>
+  );
+};
+
+export default Hero;
